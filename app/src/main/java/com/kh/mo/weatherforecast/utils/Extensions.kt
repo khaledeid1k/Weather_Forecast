8e2356040932d34de2426836ef6710a2e313946f
khaledeid1k <khaled.eid1k1k@gmail.com>
2023-11-03 16:28:45 +0200

    create inti dialog

--- app/src/main/java/com/kh/mo/weatherforecast/utils/Extensions.kt
+++ app/src/main/java/com/kh/mo/weatherforecast/utils/Extensions.kt
@@ -1,2 +1,10 @@
 package com.kh.mo.weatherforecast.utils
 
+import android.content.Context
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+
+fun createDialog(context: Context, view: Int) {
+    MaterialAlertDialogBuilder(context)
+        .setView(view)
+        .show()
+}
