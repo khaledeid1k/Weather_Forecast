2ccd7238b010a72dee7b2e519aa59097a35f6efa
khaledeid1k <khaled.eid1k1k@gmail.com>
2023-11-04 20:42:22 +0200

    add container to main

--- app/src/main/java/com/kh/mo/weatherforecast/MainActivity.kt
+++ app/src/main/java/com/kh/mo/weatherforecast/MainActivity.kt
@@ -3,10 +3,15 @@ package com.kh.mo.weatherforecast
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.NavigationUI.setupWithNavController
+import androidx.navigation.ui.navigateUp
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.kh.mo.weatherforecast.databinding.ActivityMainBinding
 import com.kh.mo.weatherforecast.local.LocalDataImp
 import com.kh.mo.weatherforecast.remot.RemoteDataImp
 import com.kh.mo.weatherforecast.repo.RepoIm
@@ -15,30 +20,28 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    lateinit var   binding : ActivityMainBinding
 
     lateinit var controller: NavController
     lateinit var bottomNavigationView: BottomNavigationView
     lateinit var navHostFragment: NavHostFragment
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        setUpBottomNavigationView()
-        val repoImInstance = RepoIm.getRepoImInstance(
-            LocalDataImp.getLocalDataImpInstance(this),
-            RemoteDataImp
+        binding  = DataBindingUtil.setContentView(
+            this,R.layout.activity_main
         )
-        CoroutineScope(Dispatchers.IO).launch {
-            val currentTemperature = repoImInstance?.getCurrentTemperature()
-            Log.d("TAG", "onCraaaaaeate:$currentTemperature ")
-        }
+        setUpBottomNavigationView()
     }
 
-    fun setUpBottomNavigationView() {
-        bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+    private fun setUpBottomNavigationView() {
+       bottomNavigationView = findViewById(R.id.bottom_navigation)
         navHostFragment = supportFragmentManager
             .findFragmentById(R.id.fragmentContainerView) as NavHostFragment
         controller = navHostFragment.navController
         setupWithNavController(bottomNavigationView, controller)
+
     }
+
+
 
 }
