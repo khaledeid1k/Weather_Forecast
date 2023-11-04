361a2092f9a128b78eb8e4e9cf3e4d0e7669e753
khaledeid1k <khaled.eid1k1k@gmail.com>
2023-11-04 20:43:39 +0200

    add fragments to nav graph

--- app/src/main/java/com/kh/mo/weatherforecast/model/Favorite.kt
+++ app/src/main/java/com/kh/mo/weatherforecast/model/Favorite.kt
@@ -1,7 +1,11 @@
 package com.kh.mo.weatherforecast.model
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity("Favorite_Table")
-data class Favorite(val nameOfCity:String) {
-}
+data class Favorite(
+    @PrimaryKey(autoGenerate = true)
+     val id: Int,
+    val nameOfCity: String
+)
---
+++ app/src/main/java/com/kh/mo/weatherforecast/ui/favorites/FavouriteFragment.kt
@@ -0,0 +1,25 @@
+package com.kh.mo.weatherforecast.ui.favorites
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
+import com.kh.mo.weatherforecast.R
+
+
+class FavouriteFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+
+        return inflater.inflate(R.layout.fragment_favourite, container, false)
+    }
+
+
+}
