4e17aff3682c54c3277a59cca5b9f6787e0df128
khaledeid1k <khaled.eid1k1k@gmail.com>
2023-11-06 01:52:52 +0200

    change houly model

--- app/src/main/java/com/kh/mo/weatherforecast/MainActivity.kt
+++ app/src/main/java/com/kh/mo/weatherforecast/MainActivity.kt
@@ -1,36 +1,31 @@
 package com.kh.mo.weatherforecast
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.NavigationUI.setupWithNavController
-import androidx.navigation.ui.navigateUp
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.kh.mo.weatherforecast.databinding.ActivityMainBinding
-import com.kh.mo.weatherforecast.local.LocalDataImp
-import com.kh.mo.weatherforecast.remot.RemoteDataImp
-import com.kh.mo.weatherforecast.repo.RepoIm
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import com.kh.mo.weatherforecast.utils.createDialog
 
 class MainActivity : AppCompatActivity() {
-    lateinit var   binding : ActivityMainBinding
-
-    lateinit var controller: NavController
-    lateinit var bottomNavigationView: BottomNavigationView
-    lateinit var navHostFragment: NavHostFragment
+    private lateinit var   binding : ActivityMainBinding
+    private lateinit var controller: NavController
+    private  lateinit var bottomNavigationView: BottomNavigationView
+    private lateinit var navHostFragment: NavHostFragment
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding  = DataBindingUtil.setContentView(
             this,R.layout.activity_main
         )
         setUpBottomNavigationView()
+       // requestInitionSetUp()
+
+    }
+    private fun requestInitionSetUp(){
+        createDialog(this,R.layout.inition_set_up)
     }
 
     private fun setUpBottomNavigationView() {
@@ -41,6 +36,8 @@ class MainActivity : AppCompatActivity() {
         setupWithNavController(bottomNavigationView, controller)
 
     }
+
+
 
 
 
