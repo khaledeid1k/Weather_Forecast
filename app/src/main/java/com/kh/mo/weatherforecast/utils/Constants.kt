76fb3c192a6ea151f8001972a60d6919f9888480
khaledeid1k <khaled.eid1k1k@gmail.com>
2023-11-06 18:16:55 +0200

    add util fun

--- app/src/main/java/com/kh/mo/weatherforecast/utils/Constants.kt
+++ app/src/main/java/com/kh/mo/weatherforecast/utils/Constants.kt
@@ -3,5 +3,13 @@ package com.kh.mo.weatherforecast.utils
 object Constants {
     const val DATA_BASE="DATA_BASE"
     const val PERMISSION_ID = 1
+    const val NOTIFICATION = "notification"
+    const val WAY_LOCATION = "selectLocation"
+    const val FIRST_TIME = "FIRST_TIME"
+    const val PREFERENCE_NAME = "PREFERENCE_NAME"
+    const val MAP = "MAP"
+    const val GPS = "GPS"
+
+
 
 }
--- app/src/main/java/com/kh/mo/weatherforecast/utils/Extensions.kt
+++ app/src/main/java/com/kh/mo/weatherforecast/utils/Extensions.kt
@@ -1,8 +1,11 @@
 package com.kh.mo.weatherforecast.utils
 
 import android.content.Context
+import android.view.View
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.Glide
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import java.util.Date
@@ -24,4 +27,21 @@ fun Long.convertUnixTimestampToDateTime(): String {
 
     val dateTime = Date(this * 1000L) // Convert seconds to milliseconds
     return dateFormat.format(dateTime)
+}
+
+
+fun Fragment.closeFragment() {
+    this.findNavController().popBackStack()
+}
+
+fun View.makeVisible() {
+    this.visibility = View.VISIBLE
+}
+
+fun View.makeGone() {
+    this.visibility = View.GONE
+}
+
+fun View.makeInVisible() {
+    this.visibility = View.INVISIBLE
 }
