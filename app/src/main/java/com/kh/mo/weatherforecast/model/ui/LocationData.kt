65655f53fd5eb71eb81f5c62183db60fe9a75035
khaledeid1k <khaled.eid1k1k@gmail.com>
2023-11-07 02:26:34 +0200

    make location data parcelable

--- app/src/main/java/com/kh/mo/weatherforecast/model/ui/LocationData.kt
+++ app/src/main/java/com/kh/mo/weatherforecast/model/ui/LocationData.kt
@@ -1,4 +1,33 @@
 package com.kh.mo.weatherforecast.model.ui
 
+import android.os.Parcel
+import android.os.Parcelable
 
-data class LocationData(val lat: Double, val lon: Double,val address:String)
+
+data class LocationData(val lat: Double, val lon: Double,val address:String?)
+    :Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readDouble(),
+        parcel.readDouble(),
+        parcel.readString()
+    ) {
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+
+    override fun writeToParcel(p0: Parcel, p1: Int) {
+    }
+
+    companion object CREATOR : Parcelable.Creator<LocationData> {
+        override fun createFromParcel(parcel: Parcel): LocationData {
+            return LocationData(parcel)
+        }
+
+        override fun newArray(size: Int): Array<LocationData?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
