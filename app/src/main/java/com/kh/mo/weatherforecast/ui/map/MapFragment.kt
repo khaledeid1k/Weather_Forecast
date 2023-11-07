ce18bf3b0a4578a0a6680030c3b8f88a78ed3716
khaledeid1k <khaled.eid1k1k@gmail.com>
2023-11-07 18:25:51 +0200

    add view model to map screen

--- app/src/main/java/com/kh/mo/weatherforecast/ui/map/MapFragment.kt
+++ app/src/main/java/com/kh/mo/weatherforecast/ui/map/MapFragment.kt
@@ -1,33 +1,24 @@
 package com.kh.mo.weatherforecast.ui.map
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.kh.mo.weatherforecast.R
-import com.kh.mo.weatherforecast.databinding.FragmentInitialBinding
 import com.kh.mo.weatherforecast.databinding.FragmentMapBinding
 import com.kh.mo.weatherforecast.local.LocalDataImp
 import com.kh.mo.weatherforecast.model.ui.LocationData
 import com.kh.mo.weatherforecast.remot.RemoteDataImp
 import com.kh.mo.weatherforecast.repo.RepoIm
-import com.kh.mo.weatherforecast.ui.initial.InitialFragmentDirections
-import com.kh.mo.weatherforecast.ui.initial.InitialViewModel
-import com.kh.mo.weatherforecast.ui.initial.InitialViewModelFactory
-import com.kh.mo.weatherforecast.utils.getAddressLocation
 
 class MapFragment : Fragment(), OnMapReadyCallback {
 
@@ -89,29 +80,26 @@ class MapFragment : Fragment(), OnMapReadyCallback {
             .setMessage(message)
             .setNegativeButton("Cancel") { dialog, _ ->
                 dialog.dismiss()
-
             }
             .setPositiveButton("Done") { dialog, _ ->
-               // changeValueOfFirstTimeOpenApp()
                 moveToHomeScreen()
                 dialog.dismiss()
             }
             .show()
     }
 
-    private fun changeValueOfFirstTimeOpenApp() {
-        mapViewModel.changeValueOfFirstTimeOpenApp(false)
-    }
 
     private fun showDialog(latLng: LatLng) {
-        getAddressLocation(
-            latLng.latitude,
-            latLng.longitude,
-            requireContext()
-        ) { _, _, nameOfCity, nameOfCountry ->
-            locationData = LocationData(latLng.latitude, latLng.longitude, nameOfCity)
+        mapViewModel.getAddressLocation(latLng.latitude,latLng.longitude){
+            nameOfCity, nameOfCountry ->
+            intiLocationData(latLng,nameOfCity)
             createDialog("$nameOfCountry , $nameOfCity")
         }
+
+    }
+
+    private fun intiLocationData(latLng: LatLng, nameOfCity: String){
+        locationData = LocationData(latLng.latitude, latLng.longitude, nameOfCity)
     }
 
     private fun moveToHomeScreen() {
--- app/src/main/java/com/kh/mo/weatherforecast/ui/map/MapViewModel.kt
+++ app/src/main/java/com/kh/mo/weatherforecast/ui/map/MapViewModel.kt
@@ -1,24 +1,14 @@
 package com.kh.mo.weatherforecast.ui.map
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.kh.mo.weatherforecast.model.ui.LocationData
-import com.kh.mo.weatherforecast.model.Weather
 import com.kh.mo.weatherforecast.repo.Repo
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
 class MapViewModel(private val repo:Repo) : ViewModel() {
 
-    fun changeValueOfFirstTimeOpenApp(isFirstTime: Boolean){
-        repo.changeValueOfFirstTimeOpenApp(isFirstTime)
+    fun getAddressLocation(lat: Double, lon: Double,
+                           getLocationData:(nameOfCity:String,
+                                            nameOfCountry:String)->Unit){
+        repo.getAddressLocation(lat, lon, getLocationData)
     }
 
 }
-
-
-
-
-
