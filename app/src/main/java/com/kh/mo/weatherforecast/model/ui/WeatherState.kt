c147d40f712435b7075421ede17e26ca3464db7c
khaledeid1k <khaled.eid1k1k@gmail.com>
2023-11-07 18:25:30 +0200

    add weather state model to show in home screen

---
+++ app/src/main/java/com/kh/mo/weatherforecast/model/ui/WeatherState.kt
@@ -0,0 +1,25 @@
+package com.kh.mo.weatherforecast.model.ui
+
+import androidx.room.Entity
+import com.kh.mo.weatherforecast.model.Daily
+import com.kh.mo.weatherforecast.model.Hourly
+
+@Entity(tableName = "WeatherState_Table", primaryKeys = ["lan", "lon"])
+data class WeatherState(
+    val lan: Double,
+    val lon: Double,
+    val nameOfCity :String,
+    val currentTime :String,
+    val temp :Double,
+    val unit :String,
+    val tempDescription :String,
+    val icon :String,
+    val humidity: Int,
+    val clouds: Int,
+    val wind_speed: Double,
+    val pressure: Int,
+    val hourly: List<Hourly>,
+    val daily: List<Daily>
+
+    )
+
