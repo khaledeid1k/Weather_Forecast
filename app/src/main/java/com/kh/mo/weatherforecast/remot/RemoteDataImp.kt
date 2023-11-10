6e3580cebdae69bbc80ed5ea7802c71f993ecdc6
khaledeid1k <khaled.eid1k1k@gmail.com>
2023-11-10 14:17:30 +0200

    get value of unit from shared preference

--- app/src/main/java/com/kh/mo/weatherforecast/remot/RemoteDataImp.kt
+++ app/src/main/java/com/kh/mo/weatherforecast/remot/RemoteDataImp.kt
@@ -4,11 +4,11 @@ import android.annotation.SuppressLint
 import android.content.Context
 import com.kh.mo.weatherforecast.local.LocalDataImp
 import com.kh.mo.weatherforecast.model.Weather
-import com.kh.mo.weatherforecast.remot.service.Network
+import com.kh.mo.weatherforecast.remot.service. Network
 import retrofit2.Response
 
-object RemoteDataImp : RemoteData {
-  private val netWork= Network.retrofitService
+class RemoteDataImp private constructor(context: Context) : RemoteData {
+  private val netWork= Network.getNetworkInstance(context).retrofitService
 
 
 
@@ -16,6 +16,18 @@ object RemoteDataImp : RemoteData {
                                                        longitude: Double): Response<Weather> {
        return netWork.getCurrentUpdatedWeatherState(latitude,longitude)
 
+    }
+    companion object{
+        @Volatile
+        private  var instance: RemoteDataImp?=null
+        fun getRemoteDataImpInstance(context: Context): RemoteDataImp {
+            return instance ?: synchronized(this){
+                val instanceHolder= RemoteDataImp(context)
+                instance =instanceHolder
+                instanceHolder
+
+            }
+        }
     }
 
 
--- app/src/main/java/com/kh/mo/weatherforecast/remot/service/ApiKeyInterceptor.kt
+++ app/src/main/java/com/kh/mo/weatherforecast/remot/service/ApiKeyInterceptor.kt
@@ -1,15 +1,21 @@
 package com.kh.mo.weatherforecast.remot.service
 
+import android.content.Context
+import android.util.Log
 import com.kh.mo.weatherforecast.BuildConfig
+import com.kh.mo.weatherforecast.local.db.sharedPref.SharedPreferencesWeather
+import com.kh.mo.weatherforecast.local.db.sharedPref.SharedPreferencesWeather.language
+import com.kh.mo.weatherforecast.local.db.sharedPref.SharedPreferencesWeather.tempUnit
 import okhttp3.Interceptor
 import okhttp3.Response
 
-class ApiKeyInterceptor : Interceptor {
+class ApiKeyInterceptor(val context: Context) : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
+        val tempUnit = SharedPreferencesWeather.customPreference(context).tempUnit.toString()
         val originalRequest = chain.request()
         val newUrl = originalRequest.url.newBuilder()
             .addQueryParameter("appid", BuildConfig.apiKey)
-            .addQueryParameter("units", "metric")
+            .addQueryParameter("units", tempUnit)
             .build()
 
         val newRequest = originalRequest.newBuilder()
