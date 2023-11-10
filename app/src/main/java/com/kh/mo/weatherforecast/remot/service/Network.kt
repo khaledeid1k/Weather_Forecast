7fab84db20f03d321516f6d44e27f14b58280375
khaledeid1k <khaled.eid1k1k@gmail.com>
2023-11-10 14:23:54 +0200

    add context parameter to network

--- app/src/main/java/com/kh/mo/weatherforecast/remot/service/Network.kt
+++ app/src/main/java/com/kh/mo/weatherforecast/remot/service/Network.kt
@@ -1,13 +1,17 @@
 package com.kh.mo.weatherforecast.remot.service
 
+import android.content.Context
+import androidx.room.Room
 import com.kh.mo.weatherforecast.BuildConfig
+import com.kh.mo.weatherforecast.local.db.WeatherDataBase
+import com.kh.mo.weatherforecast.utils.Constants
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-object Network {
+class Network  private constructor(context: Context){
     private val client = OkHttpClient.Builder()
-        .addInterceptor(ApiKeyInterceptor())
+        .addInterceptor(ApiKeyInterceptor(context))
         .build()
 
     private val retrofit: Retrofit = Retrofit.Builder()
@@ -17,5 +21,19 @@ object Network {
         .build()
     val retrofitService : Service by lazy {
         retrofit.create(Service::class.java)
+    }
+
+
+    companion object{
+        @Volatile
+        private  var instance: Network?=null
+        fun getNetworkInstance(context: Context): Network {
+            return instance ?: synchronized(this){
+                val instanceHolder= Network(context)
+                instance =instanceHolder
+                instanceHolder
+
+            }
+        }
     }
 }
