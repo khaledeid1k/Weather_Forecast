4d0b78f421d97a8e5096c56f546e6319cbf5c467
khaledeid1k <khaled.eid1k1k@gmail.com>
2023-11-11 16:39:23 +0200

    add alarm screen

--- app/src/main/java/com/kh/mo/weatherforecast/ui/alert/AlertFragment.kt
+++ app/src/main/java/com/kh/mo/weatherforecast/ui/alert/AlertFragment.kt
@@ -5,17 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.kh.mo.weatherforecast.R
+import com.kh.mo.weatherforecast.databinding.FragmentAlertBinding
+import com.kh.mo.weatherforecast.databinding.FragmentFavouriteBinding
 
 
 class AlertFragment : Fragment() {
 
 
+    private lateinit var binding: FragmentAlertBinding
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_alert, container, false)
-    }
+    ): View {
+        binding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_alert, container, false
+        )
+        return binding.root
 
+    }
 }
