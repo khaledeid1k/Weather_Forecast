d48401a6103b759e7c1db746ded910ed726d4b8e
khaledeid1k <khaled.eid1k1k@gmail.com>
2023-11-12 20:20:31 +0200

    make base diff utils for adapter

--- app/src/main/java/com/kh/mo/weatherforecast/ui/home/adapter/WeatherHourAdapter.kt
+++ app/src/main/java/com/kh/mo/weatherforecast/ui/home/adapter/WeatherHourAdapter.kt
@@ -8,6 +8,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.kh.mo.weatherforecast.R
 import com.kh.mo.weatherforecast.databinding.ItemStateOfDayBinding
 import com.kh.mo.weatherforecast.model.ui.WeatherHourData
+import com.kh.mo.weatherforecast.ui.base.BaseDataDiffUtil
 
 class WeatherHourAdapter
     : RecyclerView.Adapter<WeatherHourAdapter.WeatherHourViewHolder>() {
@@ -38,33 +39,18 @@ class WeatherHourAdapter
     }
 
     fun setItems(newItems: List<WeatherHourData>) {
-        val diffResult = DiffUtil.calculateDiff(WeatherHourDataDiffUtil(weatherHoursData, newItems))
+        val diffResult = DiffUtil.calculateDiff(
+            BaseDataDiffUtil(weatherHoursData, newItems,
+                checkItemsTheSame=    { oldItemPosition, newItemPosition -> weatherHoursData[oldItemPosition].hour == newItems[newItemPosition].hour },
+                checkContentsTheSame=  { oldItemPosition, newItemPosition -> weatherHoursData[oldItemPosition] == newItems[newItemPosition] }
+            )
+        )
         weatherHoursData = newItems
         diffResult.dispatchUpdatesTo(this)
     }
 
 
-    class WeatherHourDataDiffUtil(
-        private val oldList: List<WeatherHourData>, private val newList: List<WeatherHourData>
-    ) : DiffUtil.Callback(){
 
-
-        override fun getOldListSize()=oldList.size
-
-
-        override fun getNewListSize()=newList.size
-
-        override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
-            return oldList[oldItemPosition].temperature == newList[newItemPosition].temperature
-
-        }
-
-        override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
-            return oldList[oldItemPosition] == newList[newItemPosition]
-
-        }
-
-    }
 
 
 
--- app/src/main/java/com/kh/mo/weatherforecast/ui/home/adapter/WeatherWeekAdapter.kt
+++ app/src/main/java/com/kh/mo/weatherforecast/ui/home/adapter/WeatherWeekAdapter.kt
@@ -8,6 +8,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.kh.mo.weatherforecast.R
 import com.kh.mo.weatherforecast.databinding.ItemWeatherWeekBinding
 import com.kh.mo.weatherforecast.model.ui.WeatherWeekData
+import com.kh.mo.weatherforecast.ui.base.BaseDataDiffUtil
 
 class WeatherWeekAdapter
     : RecyclerView.Adapter<WeatherWeekAdapter.WeatherWeekViewHolder>() {
@@ -21,7 +22,8 @@ class WeatherWeekAdapter
         return WeatherWeekViewHolder(
             DataBindingUtil.inflate(
                 LayoutInflater.from(parent.context), R.layout.item_weather_week,
-                parent, false)
+                parent, false
+            )
         )
     }
 
@@ -30,40 +32,24 @@ class WeatherWeekAdapter
         holder.bind(weatherWeeksData)
     }
 
-    override fun getItemCount()=weatherWeeksData.size
+    override fun getItemCount() = weatherWeeksData.size
 
-    class WeatherWeekViewHolder (private val binding: ItemWeatherWeekBinding):
-        RecyclerView.ViewHolder(binding.root){
-        fun bind(weatherWeekData: WeatherWeekData) { binding.weatherWeekData = weatherWeekData }
+    class WeatherWeekViewHolder(private val binding: ItemWeatherWeekBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(weatherWeekData: WeatherWeekData) {
+            binding.weatherWeekData = weatherWeekData
+        }
     }
 
     fun setItems(newItems: List<WeatherWeekData>) {
-        val diffResult = DiffUtil.calculateDiff(WeatherWeekDataDiffUtil(weatherWeeksData, newItems))
+        val diffResult = DiffUtil.calculateDiff(
+            BaseDataDiffUtil(weatherWeeksData, newItems,
+                checkItemsTheSame=    { oldItemPosition, newItemPosition -> weatherWeeksData[oldItemPosition].nameOfDay == newItems[newItemPosition].nameOfDay },
+                checkContentsTheSame=  { oldItemPosition, newItemPosition -> weatherWeeksData[oldItemPosition] == newItems[newItemPosition] }
+            )
+        )
         weatherWeeksData = newItems
         diffResult.dispatchUpdatesTo(this)
-    }
-
-
-    class WeatherWeekDataDiffUtil(
-        private val oldList: List<WeatherWeekData>, private val newList: List<WeatherWeekData>
-    ) : DiffUtil.Callback(){
-
-
-        override fun getOldListSize()=oldList.size
-
-
-        override fun getNewListSize()=newList.size
-
-        override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
-            return oldList[oldItemPosition].temperature == newList[newItemPosition].temperature
-
-        }
-
-        override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
-            return oldList[oldItemPosition] == newList[newItemPosition]
-
-        }
-
     }
 
 
